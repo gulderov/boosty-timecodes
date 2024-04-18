3ce82fdd2f63bb1a618795150deb0d0d77bd200a
Danila Gulderov <danila@gulderov.me>
2024-04-18 18:41:27 +0300

    Support timecodes in comments

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,110 +1,135 @@
-const processDebounced = debounce(process, 50);
-processDebounced();
-
-const root = document.querySelector("#root");
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    if (mutation.type === "childList") {
-      processDebounced();
-    }
-  }
-});
-observer.observe(root, {
-  childList: true,
-  subtree: true,
-});
-
-function process() {
-  const posts = root.querySelectorAll('[class^="Post_content_"]');
-
+const process = debounce(() => {
+  const posts = document.querySelectorAll('[class^="Post_root_"]');
   for (const post of posts) {
     if (isShading(post)) {
       continue;
     }
 
+    attachOnClick(post);
     replaceTimecodes(post);
-    addOnClickHandlers(post);
   }
-}
+}, 100);
+
+const observer = new MutationObserver(() => {
+  process();
+});
+
+const root = document.querySelector("#root");
+observer.observe(root, {childList: true, subtree: true});
+
 
 function replaceTimecodes(post) {
   const timePattern = /(\d+):(\d+)(?::(\d+))?/g;
-  const blocks = post.querySelectorAll('[class^="BlockRenderer_markup_"]');
 
+  const blocks = post.querySelectorAll('[class^="BlockRenderer_markup_"]');
   for (const block of blocks) {
-    if (block.getAttribute("data-processed")) {
+    if (block.dataset.boostyTimecodesProcessed) {
       continue;
     }
 
     const blockText = block.innerHTML;
-    const hasTimePattern = timePattern.test(blockText);
-
-    if (!hasTimePattern) {
-      continue;
-    }
-
     const modifiedText = blockText.replace(timePattern, (match) => {
-      return `<a href="javascript:void(0)" data-timecode="${match}">${match}</a>`;
+      const anchor = document.createElement('a');
+      anchor.href = 'javascript:void(0)';
+      anchor.dataset.timecode = match;
+      anchor.classList.add('boosty-timecode');
+      anchor.textContent = match;
+
+      return anchor.outerHTML;
     });
 
-    block.setAttribute("data-processed", true);
+    block.dataset.boostyTimecodesProcessed = 'true';
     block.innerHTML = modifiedText;
   }
 }
 
-function addOnClickHandlers(post) {
-  const links = post.querySelectorAll("[data-timecode]");
+function attachOnClick(post) {
+  if (!post.dataset.boostyTimecodesListenerAttached) {
+    post.dataset.boostyTimecodesListenerAttached = 'true';
+    post.addEventListener('click', (event) => {
+      let target = event.target;
+      while (target !== event.currentTarget && !target.classList.contains('boosty-timecode')) {
+        target = target.parentNode;
+      }
 
-  for (const link of links) {
-    const timecode = link.getAttribute("data-timecode");
-    const seconds = getSeconds(timecode);
+      if (target.classList.contains('boosty-timecode')) {
+        const seconds = getSeconds(target.dataset.timecode);
 
-    link.onclick = function (event) {
-      event.preventDefault();
-      onClick(post, seconds);
-    };
+        event.preventDefault();
+        onClick(post, seconds).catch((error) => console.error('Boosty Timecodes error:', error));
+      }
+    });
   }
 }
 
-function onClick(post, seconds) {
+async function onClick(post, seconds) {
   const container = post.querySelector("[class^=VideoPlayerContainer_root_]");
-  container.scrollIntoView({ block: "center" });
+  container.scrollIntoView({block: "center"});
 
-  let videoPlayer = null;
-  let count = 1;
+  try {
+    const player = await getPlayer(container, 'vk-video-player');
+    await seek(player, seconds);
+  } catch (error) {
+    console.error('Boosty Timecodes error:', error);
+  }
+}
 
-  let intervalId = setInterval(() => {
-    console.log({ count });
-    count++;
-    videoPlayer = container.querySelector("vk-video-player");
+async function getPlayer(container, selector) {
+  let attempts = 0;
+  return new Promise((resolve, reject) => {
+    let intervalId = setInterval(() => {
+      attempts++;
+      const player = container.querySelector(selector);
+      if (player) {
+        clearInterval(intervalId);
+        resolve(player);
+      } else if (attempts > 100) {
+        clearInterval(intervalId);
+        reject();
+      }
+    }, 250);
+  });
+}
 
-    if (count > 100) {
-      clearInterval(intervalId);
+async function seek(player, seconds) {
+  try {
+    const shadowRoot = player.shadowRoot;
+    const playerWrapper = shadowRoot.querySelector(".player-wrapper");
+    const videoWrapper = playerWrapper.querySelector(".video-wrapper");
+    const previewContainer = playerWrapper.querySelector(".container");
+    let video = playerWrapper.querySelector("video");
+
+    await isVideoReady(video);
+
+    videoWrapper.classList.remove("hidden");
+    if (!previewContainer.classList.contains("hidden")) {
+      previewContainer.classList.add("hidden");
     }
 
-    if (videoPlayer) {
-      const shadowRoot = videoPlayer.shadowRoot;
-      const playerWrapper = shadowRoot.querySelector(".player-wrapper");
-      const videoWrapper = playerWrapper.querySelector(".video-wrapper");
-      const previewContainer = playerWrapper.querySelector(".container");
-      let video = playerWrapper.querySelector("video");
+    video.currentTime = seconds;
+    await video.play();
+  } catch (error) {
+    console.error('Boosty Timecodes error:', error);
+  }
+}
 
-      if (video?.readyState < 4) {
-        return;
+function isVideoReady(video) {
+  return new Promise((resolve, reject) => {
+    if (video?.readyState > 0) {
+      resolve();
+    } else {
+      function onCanPlay() {
+        video?.removeEventListener('canplay', onCanPlay);
+        resolve();
       }
 
-      clearInterval(intervalId);
-
-      videoWrapper.classList.remove("hidden");
-      if (!previewContainer.classList.contains("hidden")) {
-        previewContainer.classList.add("hidden");
-      }
-
-      video.pause();
-      video.play();
-      video.currentTime = seconds;
+      video?.addEventListener('canplay', onCanPlay);
+      setTimeout(() => {
+        video?.removeEventListener('canplay', onCanPlay);
+        reject();
+      }, 5000);
     }
-  }, 50);
+  });
 }
 
 function isShading(post) {
